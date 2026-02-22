9901ebaa227ba175f4c2439edcbf49b5881384e5
tiagok38-hash <tiagok38@gmail.com>
2026-02-21 23:15:20 -0300

    style: reduce dashboard profit card chart height to 75px

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -286,7 +286,7 @@ const ServiceOrderProfitCard: React.FC<{ serviceOrders: ServiceOrder[]; services
                 </div>
             </div>
 
-            <div className="flex-1 min-h-[85px] -mx-2">
+            <div className="flex-1 min-h-[75px] -mx-2">
                 <ResponsiveContainer width="100%" height="100%">
                     <AreaChart data={metrics.chartData}>
                         <defs>
@@ -557,7 +557,7 @@ const ProfitCard: React.FC<{ sales: Sale[]; products: Product[]; className?: str
                 </div>
             </div>
 
-            <div className="flex-1 mt-4" style={{ minHeight: '85px' }}>
+            <div className="flex-1 mt-4" style={{ minHeight: '75px' }}>
                 <ResponsiveContainer width="100%" height="100%">
                     <AreaChart data={chartData}>
                         <defs>
